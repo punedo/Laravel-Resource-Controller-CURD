850dca95d867f4be7d6bc389826fbb70104cb157
punedo <KapilKhillare@codenground.de>
2022-03-28 15:00:20 +0200

    laravel basic Operation with Resource Controller

---
+++ app/Http/Controllers/PostController.php
@@ -0,0 +1,112 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Post;
+class PostController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+     return view('insert');
+
+
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+
+   $post = new post;
+
+    $post->post_title=$request->get('title');
+    $post->post_author=$request->get('author');
+
+    $post->save();
+
+    echo "<h2 class='text-danger'>Data saved</h2>";
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Post $post)
+    {
+        //
+
+        $posts=Post::all();
+        return view('show',['posts'=>$posts]);
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+        $posts = Post::findOrFail($id);
+        return view('edit',['posts'=>$posts] );
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+        $posts = Post::findOrFail($id);
+        $posts->post_title=$request->get('title');
+        $posts->post_author=$request->get('author');
+
+
+        $posts->save();
+        return redirect('show');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+        $post = Post::findOrFail($id);
+        $post->delete();
+        return redirect('show');
+
+    }
+}
